9a406cf87e97df24383f378f6f7f1a2185373b63
piyushsatti <piyushsatti@gmail.com>
2022-01-24 15:07:14 +0530

    updated .gitignore

--- core/eval_results.py
+++ core/eval_results.py
@@ -2,7 +2,7 @@ import cv2 as cv, sys
 # imported for pathing of sub-module imports
 sys.path.append('./core/utils/')
 from utils.load_datasets import createImageDatasetObjectFromPath
-from utils.init_results import initResultsDirWithOrigAndNoisyImage
+from utils.init_results import initResultsDirWithOrigAndNoisyImage 
 
 def evaluateTestCodes():
     pass
